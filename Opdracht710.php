9afbc5ecb827f64ae5523a924957fac849026b77
Stuxles <stuxles@live.nl>
2017-03-09 16:22:37 +0100

    7,10 done

---
+++ Opdracht710.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>710</title>
+    </head>
+    <body>
+        <?php
+        $i = 1;
+        while ($i <= 100) {
+            echo "$i</br>";
+            $i += $i;
+        }
+        ?>
+    </body>
+</html>
