f136a8a2d71cc66950ab85a5afc9a3c712eab19a
Stuxles <dennisboer@gmail.com>
2017-03-30 14:23:07 +0200

    eind files fixed

--- eind/index.php
+++ eind/index.php
@@ -1,21 +1,11 @@
-<?php
-	include_once("mainIncludeGMap.php");
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php	
-			//Create a Maps object
-			//Datatypes				int	  int	  String   String
-			//setGoogleMapLocation(Width, Height, Street,  City);
-			
-			
-			setGoogleMapLocation(500, 500, "van Schaikweg 94",  "Emmen");
-        ?>
+<head>
+	<title>Interests</title>
+</head>
+<body>
+<?php
 
-    </body>
+?>
+</body>
 </html>
