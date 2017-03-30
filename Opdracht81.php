5754145ff25a4877d7ab6729df5a01b2b9bf81bf
Stuxles <dennisboer@gmail.com>
2017-03-30 13:56:22 +0200

    8.1 isset

--- Opdracht81.php
+++ Opdracht81.php
@@ -21,21 +21,22 @@
 
 
         <?php
-        $text = explode(",", $texthier);
-        echo "<h1>Output</h1>";
-        foreach ($text as $textout) {
-            //$textout = substr_replace($textout, substr($textout, 0,1), strlen($textout)-1,1);
-            $back = substr($textout, 0, 1);
-            $front = substr($textout, -1);
-            $middle = substr($textout, 1, strlen($textout)-2);
-            $textcomplete = $front .= $middle .= $back;
-            if(strlen($textout) >= 4){
-            echo "$textcomplete</br>";
-            }
-            else{
-                echo "$textout</br>";
-            }
-        }
+		if (isset($_POST["comment"])) {
+			$text = explode(",", $texthier);
+			echo "<h1>Output</h1>";
+			foreach ($text as $textout) {
+				//$textout = substr_replace($textout, substr($textout, 0,1), strlen($textout)-1,1);
+				$back = substr($textout, 0, 1);
+				$front = substr($textout, -1);
+				$middle = substr($textout, 1, strlen($textout) - 2);
+				$textcomplete = $front .= $middle .= $back;
+				if (strlen($textout) >= 4) {
+					echo "$textcomplete</br>";
+				} else {
+					echo "$textout</br>";
+				}
+			}
+		}
         ?>
     </body>
 </html>
